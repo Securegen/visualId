17896d968fec8caee7de9fcb17932a52b405709e
Caio <caiocanic@outlook.com>
2016-08-18 18:59:22 +0000

    Switched some var names

--- replaceId.sh
+++ replaceId.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-if [ -z ${CM_HOME+x} ]; then read -p "Enter the path to the build dir: " -e CM_HOME; fi
+if [ -z ${cmHome+x} ]; then read -p "Enter the path to the build dir: " -e cmHome; fi
 
-CM_WALLPAPER=$CM_HOME/vendor/cm/overlay/common/frameworks/base/core/res/res
+cmWallpaper=$cmHome/vendor/cm/overlay/common/frameworks/base/core/res/res
 
-rm -rf $CM_WALLPAPER/drawable-sw720dp-nodpi/default_wallpaper.jpg
-rm -rf $CM_WALLPAPER/drawable-hdpi/default_wallpaper.jpg
-rm -rf $CM_WALLPAPER/drawable-xhdpi/default_wallpaper.jpg
-rm -rf $CM_WALLPAPER/drawable-xxxhdpi/default_wallpaper.jpg
-rm -rf $CM_WALLPAPER/drawable-xxhdpi/default_wallpaper.jpg
-rm -rf $CM_WALLPAPER/drawable-sw600dp-nodpi/default_wallpaper.jpg
-rm -rf $CM_WALLPAPER/drawable-nodpi/default_wallpaper.jpg
+rm -rf $cmWallpaper/drawable-sw720dp-nodpi/default_wallpaper.jpg
+rm -rf $cmWallpaper/drawable-hdpi/default_wallpaper.jpg
+rm -rf $cmWallpaper/drawable-xhdpi/default_wallpaper.jpg
+rm -rf $cmWallpaper/drawable-xxxhdpi/default_wallpaper.jpg
+rm -rf $cmWallpaper/drawable-xxhdpi/default_wallpaper.jpg
+rm -rf $cmWallpaper/drawable-sw600dp-nodpi/default_wallpaper.jpg
+rm -rf $cmWallpaper/drawable-nodpi/default_wallpaper.jpg
 
-cp -rf wallpapers/default_wallpaper.jpg $CM_WALLPAPER/drawable-sw720dp-nodpi/
-cp -rf wallpapers/default_wallpaper.jpg $CM_WALLPAPER/drawable-hdpi/
-cp -rf wallpapers/default_wallpaper.jpg $CM_WALLPAPER/drawable-xhdpi/
-cp -rf wallpapers/default_wallpaper.jpg $CM_WALLPAPER/drawable-xxxhdpi/
-cp -rf wallpapers/default_wallpaper.jpg $CM_WALLPAPER/drawable-xxhdpi/
-cp -rf wallpapers/default_wallpaper.jpg $CM_WALLPAPER/drawable-sw600dp-nodpi/
-cp -rf wallpapers/default_wallpaper.jpg $CM_WALLPAPER/drawable-nodpi/
+cp -rf wallpapers/default_wallpaper.jpg $cmWallpaper/drawable-sw720dp-nodpi/
+cp -rf wallpapers/default_wallpaper.jpg $cmWallpaper/drawable-hdpi/
+cp -rf wallpapers/default_wallpaper.jpg $cmWallpaper/drawable-xhdpi/
+cp -rf wallpapers/default_wallpaper.jpg $cmWallpaper/drawable-xxxhdpi/
+cp -rf wallpapers/default_wallpaper.jpg $cmWallpaper/drawable-xxhdpi/
+cp -rf wallpapers/default_wallpaper.jpg $cmWallpaper/drawable-sw600dp-nodpi/
+cp -rf wallpapers/default_wallpaper.jpg $cmWallpaper/drawable-nodpi/
 
-CM_ANIMATION=$CM_HOME/vendor/cm/prebuilt/common/bootanimation
+cmAnimation=$cmHome/vendor/cm/prebuilt/common/bootanimation
 
-rm -rf $CM_ANIMATION/*.zip
-rm -rf $CM_ANIMATION/halfres/*.zip
+rm -rf $cmAnimation/*.zip
+rm -rf $cmAnimation/halfres/*.zip
 
-cp -R bootanimation/* $CM_ANIMATION
-cd $CM_ANIMATION/halfres
-sh $CM_ANIMATION/halfres/generate-half-res-anims.sh
+cp -R bootanimation/* $cmAnimation
+cd $cmAnimation/halfres
+sh $cmAnimation/halfres/generate-half-res-anims.sh
