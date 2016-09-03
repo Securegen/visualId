6094be77f8d13f06609af019f3ace6dda0db3493
Caio <caiocanic@outlook.com>
2016-09-03 00:29:35 +0000

    Added feedback messages
    Added messages to informe the user about what the script is doing.

--- replaceId.sh
+++ replaceId.sh
@@ -4,6 +4,7 @@ if [ -z ${cmHome+x} ]; then read -p "Enter the path to the build dir: " -e cmHom
 
 cmWallpaper=$cmHome/vendor/cm/overlay/common/frameworks/base/core/res/res
 
+echo "Replacing wallpapers..."
 rm -rf $cmWallpaper/drawable-sw720dp-nodpi/default_wallpaper.jpg
 rm -rf $cmWallpaper/drawable-hdpi/default_wallpaper.jpg
 rm -rf $cmWallpaper/drawable-xhdpi/default_wallpaper.jpg
@@ -19,12 +20,15 @@ cp -rf wallpapers/default_wallpaper.jpg $cmWallpaper/drawable-xxxhdpi/
 cp -rf wallpapers/default_wallpaper.jpg $cmWallpaper/drawable-xxhdpi/
 cp -rf wallpapers/default_wallpaper.jpg $cmWallpaper/drawable-sw600dp-nodpi/
 cp -rf wallpapers/default_wallpaper.jpg $cmWallpaper/drawable-nodpi/
+echo "Done."
 
 cmAnimation=$cmHome/vendor/cm/prebuilt/common/bootanimation
 
+echo "Replacing boot anination..."
 rm -rf $cmAnimation/*.zip
 rm -rf $cmAnimation/halfres/*.zip
 
 cp -R bootanimation/* $cmAnimation
 cd $cmAnimation/halfres
 sh $cmAnimation/halfres/generate-half-res-anims.sh
+echo "Done."
